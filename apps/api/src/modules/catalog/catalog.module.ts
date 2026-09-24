ad05bdef18a7d65ae6194e07d24745b8fb8209c4
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-09-24 23:23:17 +0300

    feat: register admin catalog controllers

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -1,12 +1,38 @@
 import { Module } from '@nestjs/common';
+import { CategoriesController } from './categories/categories.controller';
+import { CategoriesService } from './categories/categories.service';
+import { CloudinaryService } from './images/cloudinary.service';
+import { ImageUploadInterceptor } from './images/image-upload.interceptor';
 import { ImageUrlBuilder } from './images/image-urls';
+import { ImagesController } from './images/images.controller';
+import { ImagesService } from './images/images.service';
+import { MaterialTypesController } from './material-types/material-types.controller';
+import { MaterialTypesService } from './material-types/material-types.service';
 import { CatalogCache } from './public/catalog-cache';
 import { PublicCatalogController } from './public/public-catalog.controller';
 import { PublicCatalogService } from './public/public-catalog.service';
+import { RoomTypesController } from './room-types/room-types.controller';
+import { RoomTypesService } from './room-types/room-types.service';
 
 @Module({
-  controllers: [PublicCatalogController],
-  providers: [PublicCatalogService, CatalogCache, ImageUrlBuilder],
+  controllers: [
+    PublicCatalogController,
+    ImagesController,
+    RoomTypesController,
+    CategoriesController,
+    MaterialTypesController,
+  ],
+  providers: [
+    PublicCatalogService,
+    CatalogCache,
+    ImageUrlBuilder,
+    CloudinaryService,
+    ImagesService,
+    ImageUploadInterceptor,
+    RoomTypesService,
+    CategoriesService,
+    MaterialTypesService,
+  ],
   exports: [PublicCatalogService, CatalogCache],
 })
 export class CatalogModule {}
